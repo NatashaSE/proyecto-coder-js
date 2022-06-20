8e62439249a15ed8fdf1f5969fa5494c9ab434a0
Natasha Sarmiento <natashayazmin18@gmail.com>
2022-06-20 01:30:35 -0300

    Desafio complementario 2 -  Incorporar Arrays

--- js/estilo.js
+++ js/estilo.js
@@ -20,7 +20,7 @@ let cantidadPersonas = parseInt(prompt("Ingrese cantidad de personas que se van 
 
 switch (cantidadPersonas) {
     case 1:
-        alert("Hotel reservado para una persona. Total $1300 la noche")
+        alert("Hotel reservado para una persona. Total $1000 la noche")
         break;
     case 2:
         let cama = parseInt(prompt("Desea tener cama casal(1) o cama separadas(2)?"));
@@ -69,4 +69,49 @@ function calculoTotal (cantidadNoches, precioCuarto) {
 
 function calculoRealizado(cantidadNoches, precioCuarto) {
     return parseFloat(cantidadNoches) * parseFloat(precioCuarto)
+}
+
+// uso de objetivos
+
+class Habitaciones {
+    constructor( nombreCuarto, cantidadCama, capacidad, stock, precioCuarto) {
+        this.nombreCuarto = nombreCuarto;
+        this.cantidadCama = cantidadCama;
+        this.capacidad = capacidad;
+        this.stock = stock;
+        this.precioCuarto = precioCuarto;
+    }
+    precioTotal() {
+        let precioFinal = this.precioCuarto * this.cantidadNoches;
+        return precioFinal.toFixed(2);
+    };
+    descontarStock(cantidadReservadas) {
+        this.stock -= cantidadReservadas
+    }
+}
+
+const habitacion1 = new Habitaciones("Standard Doble", 1, 2, 5, 1000);
+const habitacion2 = new Habitaciones("Standard simple", 2, 2, 5, 1300);
+const habitacion3 = new Habitaciones("Standard simple", 3, 3, 4, 1350);
+
+// uso de array
+
+const Integrantes = []
+
+agregarIntegrante();
+
+function agregarIntegrante () {
+    let nombreIntegranteAagregar = prompt("Ingrese el nombre del integrante que desea agregar:")
+    let apellidoIntegranteAagregar = prompt("Ingrese el apelido del integrante que desea agregar:")
+    let resultado = Integrantes.push ({nombre: nombreIntegranteAagregar, apellido: apellidoIntegranteAagregar})
+    alert ("se ha agregado el integrante: " + resultado)
+}
+
+//uso de for....of
+listarIntegrantes();
+
+function listarIntegrantes() {
+    for (let integrante of Integrantes) {
+        console.table(Integrantes)
+    }
 }
